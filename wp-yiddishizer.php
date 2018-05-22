5e5067b1af04bc6ba1455ee4930ad48888a50bed
Gilad Ehven <gilad@ehven.com>
2018-05-22 17:09:35 -0400

    First localhost commit: Base file system (including untested [composer.json]); parallels 'wordpress-plugin-starter-type-four'.

---
+++ wp-yiddishizer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * WordPress plugin that adds Hebrew strings (via shortcodes and/or admin screens) to various WordPress types and parts
+ *
+ * @author                  Gilad Ehven
+ * @copyright               2018 Gilad Ehven (Ehventerprise LLC)
+ * @license                 GNU General Public License 2.0+
+ * @link                    https://github.com/GiladEhven/wp-yiddishizer
+ * @package                 WpYiddishizer
+ * @since                   0.1.0
+ *
+ * @wordpress-plugin
+ *
+ * Plugin Name:             WP Yiddishizer
+ *
+ * Author:                  Gilad Ehven
+ * Author URI:              http://gilad-ehven.com
+ * Contributors:            Mihal Ehven (Ehventerprise LLC)
+ * Description:             This plugin adds Hebrew strings (via shortcodes and/or admin screens) to various WordPress types and parts.
+ * Domain Path:             /languages
+ * License:                 GNU General Public License 2.0+
+ * License URI:             http://www.gnu.org/licenses/gpl-2.0.txt
+ * Network:                 False
+ * Plugin URI:              https://github.com/GiladEhven/wp-yiddishizer
+ * Privacy Policy:          https://gilad-ehven.com/privacy/
+ * Terms of Use:            https://gilad-ehven.com/terms/
+ * Text Domain:             wp-yiddishizer
+ * Version:                 0.1.0
+ *
+ */
+
+namespace WpYiddishizer;
+
+if ( ! defined( 'ABSPATH' ) ) exit( "Nothing to see here. Sorry. Try the home page." );
